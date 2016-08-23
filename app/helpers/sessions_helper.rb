c18ab6b03ed881abc2abcffd1d7150a30ffba0e2
vivekSriram <sriramvivek1990@gmail.com>
2016-08-23 13:28:19 +0530

    deleting users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -25,6 +25,10 @@ module SessionsHelper
     end
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
 
 	def logged_in?
 		!current_user.nil?
@@ -41,4 +45,15 @@ module SessionsHelper
 		session.delete(:user_id)
 		@current_user = nil
 	end
+
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
 end
